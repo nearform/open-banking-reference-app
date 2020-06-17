5b3ee5071cc545509fa787dcc67d64bb03fe9b5c
Sergey Kurdin <kurdin@gmail.com>
2020-06-17 16:38:30 -0400

    TypeScript errors fix after ts upgrade to 3.9.x

--- src/utils/hooks/useFadeIn.ts
+++ src/utils/hooks/useFadeIn.ts
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react'
 import { Animated } from 'react-native'
 
-export function useFadeIn(props: Animated.TimingAnimationConfig = {} as Animated.TimingAnimationConfig) {
+export function useFadeIn(props: Partial<Animated.TimingAnimationConfig> = {} as Animated.TimingAnimationConfig) {
   const opacity = useRef(new Animated.Value(0)).current
 
   useEffect(() => Animated.timing(opacity, { toValue: 1, delay: 0, useNativeDriver: true, ...props }).start(), [
